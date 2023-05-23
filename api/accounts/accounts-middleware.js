1aeddd454018d9ec016a5f6ad790d4c1551a3d20
Dustin Tatum <dtatum0826@gmail.com>
2023-05-23 04:48:03 -0500

    CheckAccoutPayload working

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -2,11 +2,18 @@ const Account = require('./accounts-model')
 
 
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  // Note: you can either write "manual" validation logic
-  // or use the Yup library (not currently installed)
-  console.log('checking middleware PAyload')
-  next()
+const error ={status:400}
+const{ name,budget} = req.body
+if(name === undefined || budget === undefined){
+  error.message = "name and budget are required"
+}else if( name.trim().length > 100 || name.trim().length < 3){
+  error.message = "name of account must be between 3 and 100"
+}else if(typeof budget !== 'number' || isNaN(budget) ){
+  error.message = "budget of account must be a number"
+}else if( budget < 0 || budget > 1000000){
+  error.message = "budget of account is too large or too small"
+}
+  next(error)
 
 }
 
@@ -21,7 +28,7 @@ exports.checkAccountId = async (req, res, next) => {
 const account = await Account.getById(req.params.id)
 if (!account){
   next({
-    status:404, message:'Not found'
+    status:404, message:"account not found"
   }) 
 }else{
   req.account = account
